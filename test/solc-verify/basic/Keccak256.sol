bf2f67d1f05cd3b32f95633c3e4ae5c36b039fec
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-09-12 18:48:07 -0400

    fix whitespace

--- test/solc-verify/basic/Keccak256.sol
+++ test/solc-verify/basic/Keccak256.sol
@@ -2,7 +2,7 @@ pragma solidity >=0.5.0;
 
 contract Keccak256 {
 
-  /// @notice postcondition ok 
+  /// @notice postcondition ok
   function hashTwice(bytes memory input) public pure returns (bool ok) {
     bytes32 h1 = keccak256(input);
     bytes32 h2 = keccak256(input);
