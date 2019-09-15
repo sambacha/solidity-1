a13c604a76349c45b4233aaea0c289d303ef6033
Akos Hajdu <hajdua@mit.bme.hu>
2019-09-15 00:35:10 -0700

    Fix test

--- test/solc-verify/basic/Partial.sol
+++ test/solc-verify/basic/Partial.sol
@@ -14,14 +14,15 @@ contract Partial {
         }
     }
 
+    // Last postcondition should fail
     /// @notice modifies x
     /// @notice modifies y
     /// @notice modifies address(this).balance
+    /// @notice postcondition x != y
     function() external payable {
         x = 1;
         y = 2;
         unsupported();
         assert(x == y); // Should hold
-        assert(x != y); // Should fail
     }
 }
