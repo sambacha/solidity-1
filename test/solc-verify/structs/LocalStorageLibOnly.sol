bab9d24fa621fa4e858537fd4a18ee36f47de937
Dejan Jovanovic <dejan.jovanovic@sri.com>
2019-09-19 11:47:30 -0400

    playing with nice example

--- test/solc-verify/structs/LocalStorageLibOnly.sol
+++ test/solc-verify/structs/LocalStorageLibOnly.sol
@@ -1,10 +1,21 @@
 pragma solidity >=0.5.0;
 
 library L {
-    struct S { int a; }
 
-    /// @notice postcondition self.a >= 0
+    struct S { int x; }
+
+    /// @notice postcondition self.x >= 0
     function square(S storage self) public {
-        self.a *= self.a;
+        self.x *= self.x;
+    }
+
+    /**
+     * @notice postcondition a.x == __verifier_old_int(b.x) || a.x == 0
+     * @notice postcondition b.x == __verifier_old_int(a.x) || b.x == 0
+     */
+    function swap_if_different(S storage a, S storage b) public {
+        a.x = a.x + b.x;
+        b.x = a.x - b.x;
+        a.x = a.x - b.x;
     }
 }
