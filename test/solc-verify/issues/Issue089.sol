85f7e83fe4c5e39b7b922dccea6764a6226c4038
Akos Hajdu <hajdua@mit.bme.hu>
2019-10-01 13:17:05 +0200

    Add example for issue

---
+++ test/solc-verify/issues/Issue089.sol
@@ -0,0 +1,22 @@
+pragma solidity >=0.5.0;
+contract Issue089 {
+    function sign(int x) public pure returns (string memory result) {
+        result = x > 0 ? "pos" : "neg";
+    }
+    function sign2(int x) public pure returns (string memory result) {
+        string memory pos = "pos";
+        result = x > 0 ? pos : "neg";
+    }
+
+    struct S {
+        int x;
+    }
+    S s1;
+
+    function f(int x) public {
+        s1.x = 1;
+        S memory sm = S(2);
+        sm = x > 0 ? s1 : sm;
+        s1 = x > 0 ? s1 : sm;
+    }
+}
