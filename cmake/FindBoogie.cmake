e4b25b08ab64dd97eb3043455f592da2997ebd61
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-03-19 11:45:59 -0400

    [boogie] attempt to use the global 'boogie' command if available (simplifies macos builds)

--- cmake/FindBoogie.cmake
+++ cmake/FindBoogie.cmake
@@ -1,8 +1,24 @@
+# Try to find the boogie binary itself
 if (BOOGIE_BIN)
-  find_file(BOOGIE_EXE BoogieDriver.dll "${BOOGIE_BIN}")
+  find_file(BOOGIE_EXE boogie "${BOOGIE_BIN}" DOC "Boogie executable to use")
 else()
-  find_file(BOOGIE_EXE BoogieDriver.dll)
+  find_file(BOOGIE_EXE boogie HINTS ENV PATH DOC "Boogie executable to use")
 endif()
+
+# If we couldn't find the boogie binary, try to find the DLL to run with dotnet
+if (NOT BOOGIE_EXE)
+  if (BOOGIE_BIN)
+    find_file(BOOGIE_DLL BoogieDriver.dll "${BOOGIE_BIN}" DOC "Boogie DLL to use")
+  else()
+    find_file(BOOGIE_DLL boogie DOC "Boogie DLL to use")
+  endif()
+  # If found, prepend 'dotnet' and add to BOOGIE_EXE
+  if (BOOGIE_DLL)
+    set(BOOGIE_EXE "dotnet ${BOOGIE_DLL}" CACHE STRING "Boogie executable to use")
+  endif()
+endif()
+
+message(STATUS "EXE: ${BOOGIE_EXE}")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(BOOGIE DEFAULT_MSG BOOGIE_EXE)
