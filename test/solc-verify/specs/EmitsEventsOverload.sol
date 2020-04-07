fed2554030c5ab9362f3d560e72fed359baf3e64
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-04-07 10:33:27 -0400

    [events] extending the overload example

--- test/solc-verify/specs/EmitsEventsOverload.sol
+++ test/solc-verify/specs/EmitsEventsOverload.sol
@@ -1,19 +1,30 @@
 pragma solidity >=0.5.0;
 
 contract Base {
-    event ev(int x);
+    int x;
+
+    /// @notice postcondition x > 0
+    /// @notice postcondition _x == x
+    event ev(int _x);
 }
 
 contract Derived is Base {
-    event ev(int x, int y);
+    int y;
+
+    /// @notice postcondition x > 0 && y > 0
+    /// @notice postcondition x == _x && y == _y
+    event ev(int _x, int _y);
 
     /// @notice emits ev
     function f() public {
+        x = 1;
+        y = 2;
         emit ev(1, 2);
     }
 
     /// @notice emits ev
     function g() public {
+        x = 2;
         emit ev(1);
     }
 
