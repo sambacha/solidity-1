0bae446d335ef136e8d95fb692bcd9fd13523d5b
Dejan Jovanovic <dejan.jovanovic@sri.com>
2020-04-08 15:25:44 -0400

    [quantifiers] attempt to fix format errors

--- libsolidity/boogie/ASTBoogieConverter.cpp
+++ libsolidity/boogie/ASTBoogieConverter.cpp
@@ -371,7 +371,7 @@ bool ASTBoogieConverter::parseExpr(string exprStr, ASTNode const& _node, ASTNode
 						auto arrayExpr = ASTBoogieExpressionConverter(m_context).convert(*specInfo.arrayId, false).expr;
 						auto arrayLength = m_context.getArrayLength(arrayExpr, arrayBaseTypeBg);
 						auto const &bindings = bgQuantifierVars.back();
-						for (auto const &b : bindings)
+						for (auto const& b: bindings)
 						{
 							guards.push_back(bg::Expr::lte(bg::Expr::lit((unsigned) 0), b.id));
 							guards.push_back(bg::Expr::lt(b.id, arrayLength));
